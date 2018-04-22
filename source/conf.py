0d2190a518ba1697b9569d3dc605e0b60aedd9d8
Shinsuke Nishio <nishio@densan-labs.net>
2018-04-22 17:51:30 +0900

    remove rtd

--- source/conf.py
+++ source/conf.py
@@ -235,7 +235,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'timediatemplate', 'DensanLabsTemplate Documentation',
+    ('index', 'DensanLabsTemplate Documentation',
      ['DensanLabs'], 1)
 ]
 
