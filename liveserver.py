89a614098d94197cd507333500d53bbc56428363
Shinsuke Nishio <nishio@densan-labs.net>
2018-04-22 17:22:33 +0900

    fix livereload command

--- liveserver.py
+++ liveserver.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+import os
+
+os.system('make html')
+
 from livereload import Server, shell
 
 server = Server()
-server.watch('src/**/*.rst', shell('make html'))
-server.serve(open_url=False)
+server.watch('source/**/*.rst', shell('make html'))
+server.watch('source/**/*.md', shell('make html'))
+server.serve(
+        open_url=False,
+        root='build/html',
+        port=8080,
+        host='0.0.0.0'
+        )
